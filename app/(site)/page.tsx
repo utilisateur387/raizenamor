eb0539f945b455c4ad879419ab803588f91cef81
miebi <iyeyemim@gmail.com>
2024-04-17 21:01:11 +0200

    Home: WIP bezier borders

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -4,7 +4,6 @@ import Link from 'next/link';
 
 export default async function Home() {
   const content = await getHomepage()
-  console.log(content);
   const {
     headline, 
     introParagraph, 
@@ -15,70 +14,84 @@ export default async function Home() {
     backgroundImage1, 
     backgroundImage2,
   } = content;
-  console.log(testimonials);
 
-  
   return (
-    <main className="section min-h-[90vh]">
+    <main className="min-h-[90vh]">
 
       {/* BANNER */}
-      <section className="flex space-x-6">
-        {/* <div className='w-6/12'>
-          <Image 
-            src={headshot?.url} 
-            alt={headshot?.alt}
-            width={0}
-            height={0}
-            className='w-full'
-            sizes='(max-width: 768px) 100vw, 40vw'
-            />
-        </div> */}
-        <div className="w-6/12" style={{ 
-          backgroundImage: `url(${headshot?.url}`,
-          backgroundPosition: 'center',
-          backgroundSize: 'cover'
-          }}></div>
-        <div className='w-6/12 flex items-start justify-center flex-col gap-5 section-padding'>
-          <h1 className='uppercase'>{headline}</h1>
-          <p>{introParagraph}</p>
-          <Link href='/about' className='btn'>Learn more</Link>
+
+      <div className="outside">
+        <div className="inside">
+          <section className="home-banner flex space-x-6 w-full">
+            {/* <div className='w-6/12'>
+              <Image 
+                src={headshot?.url} 
+                alt={headshot?.alt}
+                width={0}
+                height={0}
+                className='w-full'
+                sizes='(max-width: 768px) 100vw, 40vw'
+                />
+            </div> */}
+            <div className="w-5/12" style={{ 
+              backgroundImage: `url(${headshot?.url}`,
+              backgroundPosition: 'center',
+              backgroundSize: 'cover'
+              }}></div>
+
+            <div className="home-banner-intro w-7/12 flex items-center justify-center flex-col section-padding px-12">
+              <div className='flex gap-5 flex-col items-start'>
+                <h1 className='uppercase'>{headline}</h1>
+                <p>{introParagraph}</p>
+                <Link href='/about' className='btn'>Learn more</Link>
+              </div>
+            </div>
+          </section>
         </div>
-      </section>
+      </div>
+
+
+      <svg width="0" height="0">
+      <defs>
+        <clipPath id="myCurve" clipPathUnits="objectBoundingBox">
+          <path
+            d="M 0,0.9
+            L 0,0
+            C 0.2,0 0.2,0.1 1,0
+            L 1,0.96
+            C 0.6 0.8, .35 1, 0 1
+            Z"
+          />
+        </clipPath>
+      </defs>
+    </svg>
 
       {/* MISSION */}
-      <section className='section-padding'>
+      <section className='section section-padding'>
         <p className='text-center quote'>{quote}</p>
       </section>
 
 
       {/* WORK WITH ME */}
-      <section className="flex">
+      <section className="flex section">
         <div className="w-6/12 flex flex-col items-start justify-center gap-5 section-padding">
           <h2 className='uppercase'>Work with me</h2>
           <p>{workWithMe}</p>
           <Link href='/work-with-me' className='btn'>Discover my offerings</Link>
         </div>
+
+        {/* BACKGROUND IMAGE */}
         <div className="w-6/12" style={{ 
           backgroundImage: `url(${backgroundImage1.url}`,
           backgroundPosition: 'center',
           backgroundSize: 'cover'
           }}>
-          {/* BACKGROUND IMAGE */}
-          {/* <Image 
-            src={backgroundImage1.url || ''} 
-            alt={backgroundImage1.alt || ''} 
-            width={0}
-            height={0}
-            className='w-full'
-            sizes='100vw'
-          /> */}
-          --
         </div>
       </section>
 
 
       {/* TESTIMONIALS */}
-      <div className="flex section-padding">
+      <div className="flex section-padding section">
         {testimonials.map(testimonial => (
           <div className='text-center' key={testimonial._key}>
             <p>{testimonial.text}</p>
